22f3598297dbf26c690dbab122ec9f1dbde3ee56
marieapellLernia <marie.apell@lerniastudent.se>
2025-09-22 21:11:08 +0200

    added changestatus and trashcan

--- main.js
+++ main.js
@@ -6,29 +6,40 @@ const inputTodo = document.querySelector("#inputTodo"); //label
 const addBtn = document.querySelector("#addBtn"); //button
 const infoText = document.querySelector("#infoText"); //small text
 const todoList = document.querySelector("#todoList"); // ul list
-let todoText = "";
+let todoText = ""; //text thats adds in label
 let completed = 0;
-const allTheTodos = [];
+const theTodoArray = [];
 
 addBtn.addEventListener("click", addTodo);
+function changeStatus(todoText, changeStatus){
+    
+    let findIndex = theTodoArray.map(t => t.name).indexOf(todoText);
+    theTodoArray[findIndex].completed = changeStatus;
+
+};
 
 function addTodo(){
 //things that will happen when clicking add button
 
-todoText = inputTodo.value;
 infoText.textContent = "";
+todoText = inputTodo.value;
+
 if(todoText.length == 0){
    infoText.textContent = "Du kan inte ha tomma ToDo's i en ToDo lista"
    return;
 }
 
-//add the todo to the todo-array
-allTheTodos.push(todoText)
+//add the todo to the todo-array 
+const todoObject = {name: todoText, completed: false};
+theTodoArray.push(todoObject);
 
-const item = document.createElement('li');
+
+const item = document.createElement("li");
 todoList.appendChild(item);
 
-const itemText = document.createElement('span');
+
+//Add todo text as a span on the li
+const itemText = document.createElement("span");
 itemText.innerText = todoText;
 
 // add eventlistner to span with text
@@ -37,18 +48,22 @@ itemText.addEventListener("click",
     function(){
         if(item.classList.contains("completed"))
         {
-            item.setAttribute('class', "");
+            item.setAttribute("class", "");
             completed--;
             completedCount.textContent = "Du har " + completed + " avklarade ToDo's."
+            changeStatus(itemText.innerText, false)
 
         }
             
         else 
         {
-            item.setAttribute('class', "completed");
+            item.setAttribute("class", "completed");
             completed++;
             completedCount.textContent = "Du har " + completed + " avklarade ToDo's."
+            changeStatus(itemText.innerText, true)
+        
         }
+
 
 
     }
@@ -57,6 +72,23 @@ itemText.addEventListener("click",
 
 item.appendChild(itemText);
 
+//Add traschcan as a span on li
+const trashcan = document.createElement("span");
+trashcan.innerHTML = "&#128465;";
+trashcan.classList.add("traschcan");
+
+trashcan.addEventListener("click", function(){
+    item.remove();
+    let removeText = item.firstChild.textContent;
+    let indexToRemove = theTodoArray.map(t => t.name).indexOf(removeText);
+    theTodoArray.splice(indexToRemove, 1);
+    completed--;
+    completedCount.textContent = "Du har " + completed + " avklarade ToDo's."
+
+    
+})
+
+item.appendChild(trashcan);
 
 //empty input
 inputTodo.value = "";
